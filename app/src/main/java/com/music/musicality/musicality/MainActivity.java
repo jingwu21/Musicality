70331d89415783d52b8f70a1df9b942ff879bf4c
jingwu21 <jingwu721@gmail.com>
2019-11-16 16:39:39 -0600

    added components to layout

--- app/src/main/java/com/music/musicality/musicality/MainActivity.java
+++ app/src/main/java/com/music/musicality/musicality/MainActivity.java
@@ -2,12 +2,31 @@ package com.music.musicality.musicality;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView title;
+    private ImageView picture;
+    private SeekBar bar;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUp();
+
+    }
+    private void setUp(){
+        title = findViewById(R.id.musicTitle);
+        picture = findViewById(R.id.musicImage);
+        bar =  findViewById(R.id.musicBar);
+        playButton = findViewById(R.id.musicPlay);
+
     }
 }
