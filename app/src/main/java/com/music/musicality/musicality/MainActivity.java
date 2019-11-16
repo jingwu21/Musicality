62852299f26457fc6917c2cd8ecd033fcce268b4
jingwu21 <jingwu721@gmail.com>
2019-11-16 17:27:27 -0600

    prototype working version

--- app/src/main/java/com/music/musicality/musicality/MainActivity.java
+++ app/src/main/java/com/music/musicality/musicality/MainActivity.java
@@ -3,6 +3,7 @@ package com.music.musicality.musicality;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -14,19 +15,25 @@ public class MainActivity extends AppCompatActivity {
     private ImageView picture;
     private SeekBar bar;
     private Button playButton;
-
+    private MediaPlayer musicPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUp();
-
+        musicPlayer = MediaPlayer.create(this, R.raw.test);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicPlayer.start();
+            }
+        });
     }
     private void setUp(){
         title = findViewById(R.id.musicTitle);
         picture = findViewById(R.id.musicImage);
         bar =  findViewById(R.id.musicBar);
         playButton = findViewById(R.id.musicPlay);
-
+        musicPlayer = new MediaPlayer();
     }
 }
